1267ef469859b36ce006318df3505cd1c3871013
Marcos Augusto <marcos.augusto@santriweb.com.br>
2026-03-28 11:10:15 -0300

    feat: finalize initial test framework

---
+++ cypress/e2e/UI/fretes/validar_frete.cy.js
@@ -0,0 +1,16 @@
+describe("Validar fretes - desktop", () => {
+  it("validar shipping", () => {});
+
+  it('validar price', () => {
+  
+  });
+});
+
+describe("Validar fretes - mobile", () => {
+  it("validar shipping", () => {});
+
+  it('validar price', () => {
+  
+  });
+});
+
