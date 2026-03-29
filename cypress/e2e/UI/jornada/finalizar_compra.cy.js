87168cbadd8040a510dc96b9a70bd3d43181e516
Marcos Augusto <marcos.augusto@santriweb.com.br>
2026-03-29 09:32:20 -0300

    feat: finalize first skeleton of the main test

---
+++ cypress/e2e/UI/jornada/finalizar_compra.cy.js
@@ -0,0 +1,11 @@
+describe('Fluxo completo de compra do cliente', () => {
+
+    it('Deve finalizar a compra - desktop', () => {
+    
+    });
+
+    it('Deve finalizar a compra - mobile', () => {
+    
+    });
+
+});
